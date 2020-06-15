a1605cd39e8ab5679ad3fde37a4f23a96d5a67e9
Shokry <shokry92@gmail.com>
2020-06-15 17:16:28 +0200

    Add socket rooms support

--- socket.js
+++ socket.js
@@ -1,28 +1,41 @@
 const io = require("socket.io");
+const randomString = require("secure-random-string");
 
 function init(httpServer) {
   const socketServer = io(httpServer);
 
   socketServer.on("connection", function (socket) {
-    socket.on("library/get", function () {
-      socket.broadcast.emit("library/get");
-    });
-    socket.on("library/set", function (movies) {
-      socket.broadcast.emit("library/set", movies);
+    if (!socket.handshake.query.room) {
+      //Desktop
+      const room = randomString({ length: 4, alphanumeric: true });
+      socket.join(room);
+      socketServer.emit("roomName", room);
+    } else {
+      // Web (controller)
+      const { room } = socket.handshake.query;
+      socket.join(room);
+      socketServer.emit("clientConnected", room);
+    }
+
+    socket.on("library/get", function (clientRoom) {
+      socket.to(clientRoom).broadcast.emit("library/get");
     });
 
-    socket.on("trailer/watch", function (movie) {
-      socket.broadcast.emit("trailer/watch", movie);
-    });
-    socket.on("movie/watch", function (movie) {
-      socket.broadcast.emit("movie/watch", movie);
+    socket.on("library/set", function (movies, clientRoom) {
+      socket.to(clientRoom).broadcast.emit("library/set", movies);
     });
 
-    socket.on("play", function () {
-      socket.broadcast.emit("play");
+    socket.on("trailer/watch", function (movie, clientRoom) {
+      socket.to(clientRoom).broadcast.emit("trailer/watch", movie);
     });
-    socket.on("pause", function () {
-      socket.broadcast.emit("pause");
+    socket.on("movie/watch", function (movie, clientRoom) {
+      socket.to(clientRoom).broadcast.emit("movie/watch", movie);
+    });
+    socket.on("play", function (clientRoom) {
+      socket.to(clientRoom).broadcast.emit("play");
+    });
+    socket.on("pause", function (clientRoom) {
+      socket.to(clientRoom).broadcast.emit("pause");
     });
   });
 }
